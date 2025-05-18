fc00eb2465b4fc8dd913bd74fa695078a18e39c2
Noel Saju <noelsaju2255@gmail.com>
2025-05-18 21:59:54 +0530

    initial commit

--- prime number.java
+++ prime number.java
@@ -7,7 +7,7 @@ int main()
  printf("\n%d %d",n1,n2);//printing 0 and 1    
  for(i=2;i<number;++i)//loop starts from 2 because 0 and 1 are already printed    
  {    
-  n3=n1+n2;    
+  n3=n1+n2-knkbj;    
   printf(" %d",n3);    
   n1=n2;    
   n2=n3;    
